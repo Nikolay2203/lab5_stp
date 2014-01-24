a8050f15bb02e34c4dce0d80a3dce8fc67b9d69d
Nikolay2203 <Kolyanish2203@inbox.ru>
2014-01-24 22:54:08 +0700

    first commit

---
+++ src/ru/Skrynnikov/lab5_stp/Squareable.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.Skrynnikov.lab5_stp;
+
+
+public interface Squareable {
+
+    void square();
+}
